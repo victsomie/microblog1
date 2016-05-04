aa09ec040953041219e9e8a65958fb3a3fc1803e
victsomie <victsomie@yahoo.com>
2016-05-04 12:06:57 +0300

    renamed folder and redirected to the new url

--- hello_flask/app/app.py
+++ hello_flask/app/app.py
@@ -1,10 +1,10 @@
-form flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
-@app route('/')
+@app.route('/')
 def index():
     return "Hello world"
 
 if __name__=="__main__":
-    app run(debug=True, host = "0.0.0.0")
+    app.run(debug=True, host = "0.0.0.0")
---
+++ hello_flask/run.py
@@ -0,0 +1,3 @@
+#!/home/repos/microblog/hello_flask/bin/python
+from app import app
+app.run(debug=True)
